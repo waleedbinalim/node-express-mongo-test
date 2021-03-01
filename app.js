7928db6ab2bde6e1667cf1c2e9e0474f118931df
Waleed Bin Alim <68192635+waleedbinalim@users.noreply.github.com>
2021-03-01 20:39:09 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Connection String: mongodb+srv://testuser:<password>@nodetest.yvakv.mongodb.net/nodetestdatabase?retryWrites=true&w=majority
 
 const express = require('express');
-const dbURL = //PASTE YOUR DATABASE CONNECTION STRING HERE
+const dbURL = //PASTE YOUR CONNECTION STRING
 const mongoose = require('mongoose');
-const Blog = require('./models/blog')
+const blogRoutes = require('./routes/blogRoutes')
 const app = express();
 app.set('view engine' , 'ejs');
 app.use(express.static('public'));
@@ -14,8 +14,6 @@ mongoose.connect(dbURL, { useNewUrlParser: true , useUnifiedTopology: true })
 .then(() => {app.listen(3000)})
 .catch((err) => {console.log(err)});
 
-
-
 app.get('/', (req, res) => {
       res.redirect('/blogs');
   });
@@ -23,41 +21,9 @@ app.get('/', (req, res) => {
   app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
   });
-  
-  app.get('/blogs', (req, res) => {
-    Blog.find().sort({createdAt: -1})
-    .then((result) => {res.render('index' , {title: 'All Blogs' , blogs: result})})
-    .catch((err) => {console.log(err)})
-  });
-  
-  // BLOG ROUTES================================================
-  app.post('/blogs', (req, res) => {
-    console.log(req.body);
-    const blog = new Blog(req.body)
-    blog.save()
-    .then((result) => {res.redirect('/blogs')})
-    .catch((err) => {console.log(err)})
-  });
 
-  app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-  });
-  
-  app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id
-    // console.log(id);
-    Blog.findById(id)
-    .then((result) => {res.render('details' , {blog: result , title: 'Blog Details'})})
-    .catch((err) => {console.log(err)})
-  });
-  
-  app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-      .then(result => {res.json({ redirect: '/blogs' });})
-      .catch(err => {console.log(err);});
-  });
-  
+  //BlogRoutes
+  app.use('/blogs' ,blogRoutes);
 
   
   // 404 page
